4bdb125b0628224a0c9a1d696fd50738477862aa
emersonpessoa <eps011108@gmail.com>
2025-08-11 20:16:10 -0300

    feat: Adicionado TiposDeCombustivelRepository :tada:

--- src/main/java/br/com/emersopessoa/posto_combustivel/infrastrucure/repositories/TiposDeCombustivelRepository.java
+++ src/main/java/br/com/emersopessoa/posto_combustivel/infrastrucure/repositories/TiposDeCombustivelRepository.java
@@ -2,5 +2,5 @@ package br.com.emersopessoa.posto_combustivel.infrastrucure.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TiposDeCombustivel extends JpaRepository<TiposDeCombustivel, Long> {
+public interface TiposDeCombustivelRepository extends JpaRepository<TiposDeCombustivelRepository, Long> {
 }
