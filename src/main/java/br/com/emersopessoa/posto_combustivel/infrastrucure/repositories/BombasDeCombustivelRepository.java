9d18a0e6f0774d34bddfd162ab7f4dee83e9986f
emersonpessoa <eps011108@gmail.com>
2025-08-12 13:26:14 -0300

    feat: Adicionado BombasDeCombustivelRepository :sparkler:

--- src/main/java/br/com/emersopessoa/posto_combustivel/infrastrucure/repositories/BombasDeCombustivelRepository.java
+++ src/main/java/br/com/emersopessoa/posto_combustivel/infrastrucure/repositories/BombasDeCombustivelRepository.java
@@ -1,7 +1,11 @@
 package br.com.emersopessoa.posto_combustivel.infrastrucure.repositories;
 
-import br.com.emersopessoa.posto_combustivel.infrastrucure.entities.BombasDeCombustivel;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.com.emersopessoa.posto_combustivel.infrastrucure.entities.BombasDeCombustivel;
+
 public interface BombasDeCombustivelRepository extends JpaRepository<BombasDeCombustivel, Long> {
+
 }
